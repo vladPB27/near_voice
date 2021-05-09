ae01d883a1b8573acaad2c27d902aaac5bb2d0d7
vladpb27 <vladpb27>
2021-05-09 12:52:56 -0500

    navigation added

---
+++ lib/route_generator.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:near_voice/main.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    //getting arguments passed in while calling navigator.pushnamed
+    final args = settings.arguments;
+
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(builder: (_) => InitialPage());
+      case '/second':
+        return MaterialPageRoute(builder: (_) => MyApp());
+      // case '/third':
+      //   if (args is String){
+      //     return MaterialPageRoute(
+      //         builder: (_) => InitialPage(
+      //           data:args, //data es del la otra vista
+      //         ),
+      //     );
+      //   }
+      //   return _errorRoute();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Error'),
+          ),
+          body: Center(
+          child: Text('ERROR'),
+      ),
+      );
+    });
+  }
+}
