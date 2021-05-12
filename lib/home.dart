8a09a2d6cdbe854d65a1d7c28db02cfa04b6dd52
vlad <vladpb27@gmail.com>
2021-05-12 18:05:23 -0500

    home updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:near_voice/main.dart';
+import 'package:imagebutton/imagebutton.dart';
 import 'dart:io';
 
 const _PORT = 8888;
@@ -26,6 +27,7 @@ class _InitialPageState extends State<Home> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        // backgroundColor: Colors.white,
         appBar: AppBar(
           title: Text(
             'nearvoice',
@@ -33,21 +35,41 @@ class _InitialPageState extends State<Home> {
           ),
           backgroundColor: HexColor('#006059'),
         ),
-        body: Container(
-          decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage('assets/app-04-home.jpg'),
-                  fit: BoxFit.cover)),
+        body: Center(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              TextField(
-                controller: myController,
-                decoration: InputDecoration(
-                  border: UnderlineInputBorder(),
-                  hintText: 'Enter your name'
+
+              Text('Welcome @user'),
+              MaterialButton(
+                padding: EdgeInsets.all(8.0),
+                textColor: Colors.white,
+                splashColor: Colors.greenAccent,
+                elevation: 8.0,
+                child: Container(
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage('assets/png/createss.png'),
+                        fit: BoxFit.cover),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text("SIGN OUT"),
+                  ),
                 ),
+                // ),
+                onPressed: () {
+                  print('Tapped');
+                },
+              ),
+              Expanded(child: Image.asset('assets/app-02-login.jpg',),
+              ),
+              Row(
+                children: [
+                  Expanded(child: Image.asset('assets/nearvoicefont.jpg',),
+                  ),
+                  Expanded(child: Image.asset('assets/png/createss.png',),
+                  ),
+                ],
               ),
               TextField(
                 controller: myController,
@@ -62,7 +84,8 @@ class _InitialPageState extends State<Home> {
                     color: Colors.lightGreen,
                     textColor: Colors.white,
                     onPressed: () {
-                      Navigator.of(context).pushNamed('/meetcreated',arguments: 'hi to created');
+                      Navigator.of(context).pushNamed('/meetcreated',
+                          arguments: 'hi to created');
                       _runServer();
                     },
                     child: Text('Create'),
@@ -73,10 +96,9 @@ class _InitialPageState extends State<Home> {
                     onPressed: () {
                       ipEnter = myController.text;
                       print("ip enter: $ipEnter");
-                      Navigator.of(context).pushNamed(
-                          '/meetjoin',
-                          arguments: ipEnter);
-                          // arguments: 'hello from home');
+                      Navigator.of(context)
+                          .pushNamed('/meetjoin', arguments: ipEnter);
+                      // arguments: 'hello from home');
                     },
                     child: Text('join'),
                   ),
@@ -85,6 +107,70 @@ class _InitialPageState extends State<Home> {
             ],
           ),
         ),
+        // Container(
+        //   ///image in the background
+        //   // decoration: BoxDecoration(
+        //   //     image: DecorationImage(
+        //   //         image: AssetImage('assets/app-04-home.jpg'),
+        //   //         fit: BoxFit.cover)
+        //   // ),
+        //   child: Column(
+        //     crossAxisAlignment: CrossAxisAlignment.stretch,
+        //     mainAxisAlignment: MainAxisAlignment.end,
+        //     children: [
+        //       IconButton(
+        //           icon: Image.asset('assets/png/createss.png'),
+        //           onPressed: () {}),
+        //       Text('here comes a button'),
+        //       ImageButton(
+        //         children: <Widget>[],
+        //         width: 91,
+        //         height: 36,
+        //         paddingTop: 5,
+        //         pressedImage: Image.asset(
+        //           "assets/png/creates.png",
+        //         ),
+        //         unpressedImage: Image.asset("assets/png/creates.png"),
+        //         onTap: () {
+        //           print('test');
+        //         },
+        //       ),
+        //       TextField(
+        //         controller: myController,
+        //         decoration: InputDecoration(
+        //             border: UnderlineInputBorder(),
+        //             hintText: 'example: ws://ip_address:8888'),
+        //       ),
+        //       Row(
+        //         mainAxisAlignment: MainAxisAlignment.center,
+        //         children: [
+        //           RaisedButton(
+        //             color: Colors.lightGreen,
+        //             textColor: Colors.white,
+        //             onPressed: () {
+        //               Navigator.of(context).pushNamed('/meetcreated',
+        //                   arguments: 'hi to created');
+        //               _runServer();
+        //             },
+        //             child: Text('Create'),
+        //           ),
+        //           RaisedButton(
+        //             color: Colors.amber,
+        //             textColor: Colors.white,
+        //             onPressed: () {
+        //               ipEnter = myController.text;
+        //               print("ip enter: $ipEnter");
+        //               Navigator.of(context)
+        //                   .pushNamed('/meetjoin', arguments: ipEnter);
+        //               // arguments: 'hello from home');
+        //             },
+        //             child: Text('join'),
+        //           ),
+        //         ],
+        //       ),
+        //     ],
+        //   ),
+        // ),
       ),
     );
   }
@@ -107,8 +193,9 @@ void _runServer() async {
 
   final connections = Set<WebSocket>();
   // HttpServer.bind('192.168.71.10', _PORT).then((HttpServer server) {
-    //ip static
-    HttpServer.bind(ipPhone.toString(), _PORT).then((HttpServer server) { //own ip address
+  //ip static
+  HttpServer.bind(ipPhone.toString(), _PORT).then((HttpServer server) {
+    //own ip address
     print("ip phone server: $ipPhone");
     print('[+]WebSocket listening at -- ws://ip_address:$_PORT/');
     server.listen((HttpRequest request) {
